9469afd92fc77a650dbf5835ac4f2e0805b166b3
seungcheol Oh <seung9412@naver.com>
2020-12-20 06:05:13 +0900

    initial message

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
+
+// src 폴더에서 new 누르고 class를 만든다.
+// .java가 만들어지는데
+// source file에 문제가 없으면 bin 폴더에 .class이 만들어진다.
